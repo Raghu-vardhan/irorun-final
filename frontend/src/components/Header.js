ee46a434eb23eb7834bccfd96235f2366087f767
Vignesh M <vigneshchary35@gmail.com>
2026-02-04 13:39:32 +0530

    added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <div className="header ">
       <div className="brand">
-        <a href=""><img src="https://irorun.in/cdn/shop/files/338375715_2274413929407990_2262713576497268016_n.jpg?v=1769742573&width=280" alt="logo" /></a>
+        <a href="/"><img src="https://irorun.in/cdn/shop/files/338375715_2274413929407990_2262713576497268016_n.jpg?v=1769742573&width=280" alt="logo" /></a>
       </div>
       <button onClick={logout}>Logout</button>
     </div>
