1b2515ef0c3140d64fd0dc142293dc5f791ae867
Vignesh M <vigneshchary35@gmail.com>
2026-02-04 13:02:57 +0530

    added

--- frontend/src/components/StatsCards.js
+++ frontend/src/components/StatsCards.js
@@ -8,8 +8,8 @@ const StatsCards = ({ stats }) => {
       <h1>WELCOME BACK TO IRORUN</h1>
     </div>
      <div className="stats-grid ">
-      <h1 className="card"><b>Revenue</b> ₹{stats.totalRevenue}</h1>
-      <h1 className="card"><b>Coupens Revenue</b> ₹{stats.couponRevenue}</h1>
+      <h1 className="card"><b>Revenue</b> ₹ {stats.totalRevenue}</h1>
+      <h1 className="card"><b>Saved Amout</b> ₹ {stats.couponRevenue}</h1>
       <h1 className="card"><b>Orders : </b>{stats.totalOrders}</h1>
     </div>
    </div>
