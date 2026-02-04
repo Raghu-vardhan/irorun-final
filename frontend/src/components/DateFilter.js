2bf889c78f519e8ef6964582718925c2e8032ca7
Vignesh M <vigneshchary35@gmail.com>
2026-02-04 12:57:44 +0530

    added

--- frontend/src/components/DateFilter.js
+++ frontend/src/components/DateFilter.js
@@ -1,9 +1,15 @@
 const DateFilter = ({ fromDate, toDate, setFromDate, setToDate }) => {
   return (
-    <div className="filter page-width">
+   <div className="page-width">
+   
+   <div className="heading">
+      <h2>Order History</h2>
+   </div>
+     <div className="filter page-width">
       <input type="date" value={fromDate} onChange={e => setFromDate(e.target.value)} />
       <input type="date" value={toDate} onChange={e => setToDate(e.target.value)} />
     </div>
+   </div>
   );
 };
 
